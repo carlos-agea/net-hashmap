df986630be38069607241649ea472dae4af96667
Carlos <carlos@localhost.localdomain>
2017-02-07 23:39:06 +0000

    Http code refactoring.

--- bin/http.js
+++ bin/http.js
@@ -2,9 +2,11 @@
  * Created by carlos on 2/7/17.
  */
 var http = require('http');
+
 var port = 8000;
-var callback_set;
-var callback_get;
+var urlRegex = /^\/(get|set){1}\/([a-zA-Z]+)=*([a-zA-Z0-9]+)*(.*)$/;
+var callback_set = null;
+var callback_get = null;
 
 module.exports = function (options) {
     callback_set = options.set;
@@ -12,20 +14,24 @@ module.exports = function (options) {
 };
 
 var server = http.createServer(function (request, response) {
-    var match = request.url.match(/^\/([a-zA-Z]+)\/([a-zA-Z]+)=([a-zA-Z0-9]+)\/*(.*)$/);
+    var match = request.url.match(urlRegex);
 
-    if (match !== null && (match.length === 4 || match.length === 5)) {
+    if (isRequestValid(request)) {
         var operation = match[1].toString();
-        var key = match[2].toString();
-        var value = match[3].toString();
 
         if (operation === 'set') {
+            var key = match[2].toString();
+            var value = match[3].toString();
+
             callback_set(key, value);
+
             response.writeHead(200);
-            response.end('Perfect!');
+            response.end('Saved ' + key + "=" + value);
         }
         else if (operation === 'get') {
+            var key = match[2].toString();
             var value = callback_get(key);
+
             response.writeHead(200);
             response.end(key + '=' + value);
         }
@@ -36,6 +42,29 @@ var server = http.createServer(function (request, response) {
     }
 
 }).listen(port);
+
+
+function isRequestValid(request) {
+    var match = request.url.match(urlRegex);
+    var valid = false;
+
+    if (match !== null && (match.length === 4 || match.length === 5)) {
+        var operation = match[1].toString();
+
+        if (operation === 'set') {
+            valid = true;
+
+            //TODO: filter key and value
+        }
+        else if (operation === 'get') {
+            valid = true;
+        }
+        else {
+            valid = false;
+        }
+    }
+    return valid;
+}
 
 function return404(response) {
     response.writeHead(404);
