cbdb91b8bcdebcf9f212c12c98bbf74a7a3e0860
Carlos <carlos@localhost.localdomain>
2017-02-07 22:48:28 +0000

    http modul, parsing request params.

--- bin/http.js
+++ bin/http.js
@@ -1,12 +1,29 @@
 /**
  * Created by carlos on 2/7/17.
  */
-var http = require("http");
-var server = http.createServer(function(request, response) {
-    response.writeHead(200);
+var http = require('http');
+var port = 8000;
 
-    response.end('Hello');
-});
+var server = http.createServer(function (request, response) {
+    var match = request.url.match(/^\/([a-zA-Z]+)\/([a-zA-Z]+)=([a-zA-Z0-9]+)\/*(.*)$/);
 
-server.listen(8000);
-console.log("Server is listening");
+    if (match !== null && match.length === 5) {
+        var operation = match[1].toString();
+        var value = match[2].toString();
+        var key = match[2].toString();
+
+        response.writeHead(200);
+        response.end('Perfect!');
+    }
+    else {
+        return404(response);
+    }
+    
+}).listen(port);
+
+function return404(response) {
+    response.writeHead(404);
+    response.end('Wrong request!');
+}
+
+console.log("Server is listening on port " + port);
